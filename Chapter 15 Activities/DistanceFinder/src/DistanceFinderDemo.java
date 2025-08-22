042afdc75ed8321a464cc54a3df53606a38f0b8e
Mona List <jtwang@stu.naperville203.org>
2025-08-22 13:17:07 -0500

    nonoononononono

--- Chapter 15 Activities/DistanceFinder/src/DistanceFinderDemo.java
+++ Chapter 15 Activities/DistanceFinder/src/DistanceFinderDemo.java
@@ -1,14 +1,14 @@
-import java.util.Scanner;
 public class DistanceFinderDemo
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
+        //Scanner in = new Scanner(System.in);
         System.out.print("File with city names: ");
-        String filename = in.nextLine();
-        DistanceFinder d = new DistanceFinder(filename);
+        //String filename = in.nextLine();
+        //DistanceFinder d = new DistanceFinder(filename);
 
-        System.out.println("Shortest distance from " + d.getStartingCity());
-        System.out.println(d.shortestDistances());
+        //System.out.println("Shortest distance from " + d.getStartingCity());
+        //System.out.println(d.shortestDistances());
+        System.out.println("Hello test");
     }
 }
