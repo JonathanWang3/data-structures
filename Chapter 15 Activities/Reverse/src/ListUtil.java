a046b858b43b829f3b87741242866eb5ee2d2897
Jonathan <jtwang@stu.naperville203.org>
2025-08-29 13:10:27 -0500

    nononononono
    grrrrrrr

--- Chapter 15 Activities/Reverse/src/ListUtil.java
+++ Chapter 15 Activities/Reverse/src/ListUtil.java
@@ -10,8 +10,28 @@ public class ListUtil
      *
      * @param strings the linked list to reverse
     */
-    public static void reverse(LinkedList<String> strings)
+    public static LinkedList reverse(LinkedList<String> strings)
     {
-        ...
+        LinkedList<String> og = strings;
+        LinkedList<String> fill = new LinkedList<>();
+        LinkedList<String> newL = remover(fill,og);
+        System.out.println(newL);
+        return newL;
+
+    }
+
+    public static LinkedList remover(LinkedList<String> strings, LinkedList<String> oG)
+    {
+        LinkedList<String> newList = strings;
+        if (!oG.isEmpty())
+        {
+            
+            newList.add(oG.removeLast());
+            return remover(newList, oG);
+        }
+        else
+        {
+            return newList;
+        }
     }
 }
