f0127330a6de92d504eac252ba203861798b810c
Jonathan <jtwang@stu.naperville203.org>
2025-08-29 12:35:16 -0500

    Update ListDemo.java

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,90 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /* The addLast method can be used to populate a list */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Peter");
+        staff.addLast("Steve");
+
+        System.out.println(staff);
+
+        // The list is currently TNPS
+        /* 
+         * The list iterator method creates a new list iterator
+         * that is positioned at the head of the list.
+         * The | is used to represent iterator pos
+        */
+        ListIterator<String> iterator = staff.listIterator(); //|TNPS
+
+        /* The next method advances the iterator over the next element in the list */
+        iterator.next(); // T|NPS
+
+        /* The next method also returns the element that the itertor passes over */
+        String avenger = iterator.next(); // TN|PS
+        System.out.println(avenger); // Should print Natasha
+
+        /* The add method inserts an element at the iterator position.
+         * The iterator is then positioned AFTER the element that was added
+         */
+        iterator.add("Clint"); // TNC|PS
+        iterator.add("Bruce"); // TNCB|PS
+        System.out.println(staff);
+
+        /* The remove method can only be called after calling next or previous */
+        // iterator.remove(); This line would cause an IllegalStateException
+        // The remove method removes the element returned after calling next or previous
+        iterator.next(); // TNCBP|S
+        iterator.remove(); // Peter is removed -> TNCB|S
+        System.out.println(staff);
+
+        /* The set method updates the element returned by
+         *the last call to next or previous
+         */
+        iterator.previous(); //TNC|BS
+        iterator.set("Thor"); // Bruce is replaced by Thor -> TNC|TS
+        System.out.println(staff);
+
+        /* The hasNext method is used to determine if there is a next node
+         * after the iterator. The hasNext is often used in the condition
+         * of a while loop
+         */
+        iterator = staff.listIterator(); // |TNCTS
+        while (iterator.hasNext()) {
+            String n = iterator.next();
+            if (n.equals("Natasha")) { //TN|CTS
+                iterator.remove(); // T|CTS
+            }
+        } // TCTS|
+
+        /* Enhanced for loops work with linked lists as well */
+        for (String n: staff) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+
+        /* ConcurrentModificationException
+         * 
+         * CANNOT modify a linked list while using an iterator
+         * UNLESS you use the iterator to do the modification
+         */
+        iterator = staff.listIterator(); // |TCTS
+        while (iterator.hasNext()) {
+            String n = iterator.next();
+            if (n.equals("Tony")) {
+                //staff.remove("Tony"); ConcurrentModificationException
+            }
+        }
+
+        // Embedded iterator when for loop is created
+        for (String n: staff) {
+            if (n.equals("Tony")) {
+                staff.add("T'Challa");
+            }
+        }
+
+        System.out.println(staff);
         
     }
 }
