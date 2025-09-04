55c742ddfdb8f66951562b6d230e51573ffd5cfe
Jonathan <jtwang@stu.naperville203.org>
2025-09-04 13:27:59 -0500

    notes on hashmap

--- Chapter 15 Class Notes/src/WordAnalysis.java
+++ Chapter 15 Class Notes/src/WordAnalysis.java
@@ -1,9 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
-import java.io.File;
-import java.io.FileNotFoundException;
 
 /**
  * This program checks which words in a file are not present in a dictionary.
@@ -13,6 +13,35 @@ public class WordAnalysis
     public static void main(String[] args)
         throws FileNotFoundException
     {
+        // Determine the current working directory
+        // System.out.println(System.getProperty("user.dir"));
+
+        // Read the dictionary file and the novel file
+        /*Set<String> dictionaryWords = readWords("Chapter 15 Class Notes/src/words");
+        Set<String> novelWords = readWords("Chapter 15 Class Notes\\src\\war-and-peace.txt");*/
+
+        /**/Set<String> dictionaryWords = readWords("src\\words");
+        Set<String> novelWords = readWords("src\\war-and-peace.txt");
+
+        // Print all the words that are in the novel, but not in the dictionary.
+        for (String word : novelWords) {
+            if (!dictionaryWords.contains(word)) {
+                System.out.println(word);
+            }
+        }
+
+        // Print out the number of unique words in the novel
+        System.out.println("There are "+novelWords.size()+" unique words");
+
+        // Print the number of unique words ith >3 letters
+        Iterator<String> iterator = novelWords.iterator();
+        while (iterator.hasNext()) {
+            if (iterator.next().length() <= 3) {
+                iterator.remove();
+            }
+        }
+
+        System.out.println("There are "+novelWords.size()+" unique words longer than 3 char");
     }
 
     /**
@@ -25,6 +54,19 @@ public class WordAnalysis
     public static Set<String> readWords(String filename)
         throws FileNotFoundException
     {
-        return null;
+        // We use a HashSet instead of a TreeSet because the order doesn't matter
+        Set<String> words = new HashSet<>();
+
+        Scanner in = new Scanner(new File(filename), "UTF-8");
+
+        // Use any character that's not a letter as a delimiter
+        in.useDelimiter("[^a-zA-Z]+");
+
+        while (in.hasNext()) {
+            // Add words to the set (duplicates are automatically ignored)
+            words.add(in.next().toLowerCase());
+        }
+
+        return words;
     }
 }
