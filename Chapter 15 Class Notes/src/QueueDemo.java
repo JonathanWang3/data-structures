13d857cc13f777eb1c4a9dc02b22fa0be8736581
Jonathan <jtwang@stu.naperville203.org>
2025-09-16 13:20:21 -0500

    note

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,37 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        // Add some j*bs
+        jobs.add("Harry: Quarter 2 Expense Report");
+        jobs.add("Connor: Recipe for Banana Bread");
+        jobs.add("Katherine: Top Secret Document");
+
+        System.out.println("Printing: "+jobs.remove());
+
+        // Add some more print jobs
+        jobs.add("Vardhan: Grocery List");
+        jobs.add("Katherine: Really Top Secret Document");
+        jobs.add("Katherine: Can I Get Fired For ts?");
+
+        System.out.println("Printing: "+jobs.remove());
+        System.out.println("Printing: "+jobs.remove());
+
+        jobs.add("Boss: Katherine Termination Letter");
+
+
+        // Print out the rest of the jobs in the queue
+        // regular for loop
+        /*for (String job: jobs) {
+            System.out.println(job);
+        }*/
+
+
+        while (jobs.size() > 0) {
+            System.out.println("Printing: "+jobs.remove());
+        }
+
     }
 }
