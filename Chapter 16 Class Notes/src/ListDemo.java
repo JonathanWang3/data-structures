6b456e603f7798ba441427da376394444e2e90ca
Jonathan <jtwang@stu.naperville203.org>
2025-09-26 13:17:33 -0500

    ts scioly graze the roof

--- Chapter 16 Class Notes/src/ListDemo.java
+++ Chapter 16 Class Notes/src/ListDemo.java
@@ -5,5 +5,12 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList students = new LinkedList();
+        students.addFirst("John");
+        students.addFirst("Sarah");
+        students.addFirst("Mark");
+        students.addFirst("Tanya");
+
+        System.out.println(students);
     }
 }
